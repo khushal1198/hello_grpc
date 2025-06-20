7ddbe424a9b19effbbfcbddc7840dffb9ac3216c
khushal1198 <khushal.pujara@yahoo.com>
2025-06-20 16:24:34 -0700

    feat: implement gRPC health check protocol and update dependencies

--- khushal_hello_grpc/src/server/server.py
+++ khushal_hello_grpc/src/server/server.py
@@ -2,10 +2,17 @@ import grpc
 from concurrent import futures
 from khushal_hello_grpc.src.server.impl.service_impl import HelloService
 from khushal_hello_grpc.src.generated import hello_pb2_grpc
+from grpc_health.v1 import health, health_pb2_grpc
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     hello_pb2_grpc.add_HelloServiceServicer_to_server(HelloService(), server)
+    # Add gRPC health checking
+    health_servicer = health.HealthServicer()
+    health_pb2_grpc.add_HealthServicer_to_server(health_servicer, server)
+    # Set the health status to SERVING for all services
+    health_servicer.set('', health_pb2_grpc.health_pb2.HealthCheckResponse.SERVING)
+    health_servicer.set('HelloService', health_pb2_grpc.health_pb2.HealthCheckResponse.SERVING)
     server.add_insecure_port('[::]:50051')
     print("gRPC server running on port 50051...")
     server.start()
